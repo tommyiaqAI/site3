44f3968ac4573588e81ce28f7266062f407c8f82
tommyiaqai <tommaso.iaquinta1991@gmail.com>
2025-06-15 17:44:32 +0200

    add config

---
+++ site-config.js
@@ -0,0 +1,11 @@
+export default {
+  siteId: "site3",
+  firebaseConfig: {
+    apiKey: "AIzaSyAdFeVtKdb5oczNANmYPOeTkf2WbP_ruqg",
+    authDomain: "store-products-ed162.firebaseapp.com",
+    projectId: "store-products-ed162",
+    storageBucket: "store-products-ed162.firebasestorage.app",
+    messagingSenderId: "844599226880",
+    appId: "1:844599226880:web:241eeb3accf826a7005ee5"
+  }
+};
