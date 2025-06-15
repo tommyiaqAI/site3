d04956f890f2ec31ca283d65927edeea36784544
tommyiaqAI <tommaso.iaquinta1991@gmail.com>
2025-06-15 18:11:59 +0200

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -1,5 +1,10 @@
 export default {
   siteId: "site3",
+  storeName: "My Awesome Store 3",
+  contact: {
+    email: "info@myawesomestore.com",
+    phone: "+123456789"
+  },
   firebaseConfig: {
     apiKey: "AIzaSyAdFeVtKdb5oczNANmYPOeTkf2WbP_ruqg",
     authDomain: "store-products-ed162.firebaseapp.com",
